e6b975bfbfc312cd98ef395f0380254a04b4d9c9
Garnere5 <garnere@huntington.edu>
2025-09-23 15:44:31 -0400

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,53 @@
+/// palindrome.java 
+//
+// fall 2025
+// lab4b assignment
+//
+//  Earl Garner
+//
+// Use for paindrome.java
+
+public class Palindrome {
+    
+    public static void main(String[] args) {
+
+        //single call to test
+        System.out.println( palindrome("racecar") ); //true
+        System.out.println( palindrome("sass") ); //false
+
+        //uncomment to test additional words
+        String words[] = { "a", "racecar", "noon", "hannah", "abcba", "abbca", "nope", "ab" };
+
+        for (int i = 0; i < words.length; i++) {
+
+            if (palindrome(words[i]) == true)
+                System.out.println( words[i] + " IS a palindrome");
+            else
+                System.out.println( words[i] + " IS NOT a palindrome");
+        }
+        
+    }// main
+
+    // *** use StackCharArray and QueueCharArray here to determine Palindrome
+    public static boolean palindrome(String word) {
+
+        //assume status is true
+        boolean status = true;
+        
+
+        
+        while (Stack != 0 && Queue != 0 && status == true){
+            if (Stack.top() != Queue.front()){
+                status = false;
+            } else {
+                Stack.pop();
+                Queue.remove();
+            }
+
+        }
+
+        return status;
+        
+    }// palindrome method
+
+}//class
